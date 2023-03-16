a8092cb8bf9bc8e434e69bbfc330616ee9175e60
cayde <caydebrummer@gmail.com>
2023-03-16 13:35:36 +0000

    Hope this works

--- app/src/main/java/com/example/wordotest/wordActivity.kt
+++ app/src/main/java/com/example/wordotest/wordActivity.kt
@@ -4,24 +4,39 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import wordAdapter
+import androidx.recyclerview.widget.DefaultItemAnimator
+import com.example.wordotest.R.id
 
 class wordActivity : AppCompatActivity() {
+    private val itemsList = ArrayList<String>()
+    private lateinit var customAdapter: CustomAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_word)
+        setContentView(R.layout.activity_main)
+        title = "List of words"
 
-        val recyclerView: RecyclerView = findViewById(R.id.recyclerview)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-
-        // ArrayList of class ItemsViewModel
-        val data = ArrayList<ItemsViewModel>()
-
-        data.add(ItemsViewModel("blue"))
-        val adapter = wordAdapter(data)
-
-        recyclerView.adapter = adapter
-
-
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        customAdapter = CustomAdapter(itemsList)
+        val layoutManager = LinearLayoutManager(applicationContext)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = customAdapter
+        prepareItems()
+    }
+    private fun prepareItems() {
+        itemsList.add("Item 1")
+        itemsList.add("Item 2")
+        itemsList.add("Item 3")
+        itemsList.add("Item 4")
+        itemsList.add("Item 5")
+        itemsList.add("Item 6")
+        itemsList.add("Item 7")
+        itemsList.add("Item 8")
+        itemsList.add("Item 9")
+        itemsList.add("Item 10")
+        itemsList.add("Item 11")
+        itemsList.add("Item 12")
+        itemsList.add("Item 13")
+        customAdapter.notifyDataSetChanged()
     }
 }
