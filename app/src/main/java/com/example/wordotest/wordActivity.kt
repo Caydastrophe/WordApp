8d639ab80ab9d7595ec728614017d18d56026f63
cayde <caydebrummer@gmail.com>
2023-03-16 13:46:50 +0000

    looking good

--- app/src/main/java/com/example/wordotest/wordActivity.kt
+++ app/src/main/java/com/example/wordotest/wordActivity.kt
@@ -1,11 +1,12 @@
 package com.example.wordotest
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.DefaultItemAnimator
-import com.example.wordotest.R.id
+
 
 class wordActivity : AppCompatActivity() {
     private val itemsList = ArrayList<String>()
@@ -13,7 +14,7 @@ class wordActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_word)
         title = "List of words"
 
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
@@ -24,12 +25,12 @@ class wordActivity : AppCompatActivity() {
         prepareItems()
     }
     private fun prepareItems() {
-        itemsList.add("Item 1")
-        itemsList.add("Item 2")
-        itemsList.add("Item 3")
-        itemsList.add("Item 4")
-        itemsList.add("Item 5")
-        itemsList.add("Item 6")
+        itemsList.add("blue")
+        itemsList.add("orange")
+        itemsList.add("green")
+        itemsList.add("magenta")
+        itemsList.add("cyan")
+        itemsList.add("purple")
         itemsList.add("Item 7")
         itemsList.add("Item 8")
         itemsList.add("Item 9")
